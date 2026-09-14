821daed5c363d336bd77a8117d451eb07ac9ad51
kaiodavid727-cell <kaiodavid727@gmail.com>
2026-09-14 14:39:27 -0300

    atividade mao na massa

--- 01_nocoes_e_fundamentos/olamundo.py
+++ 01_nocoes_e_fundamentos/olamundo.py
@@ -1,2 +1,11 @@
 print("kaio david")
 print("ola mundo!")
+nome = input("digite seu nome")
+dia = input("digite o dia de nascimento")
+mes = input("digite seu mes de nascimento")
+ano = input("digite o ano de nascimento")
+print(f"seu nome e{nome} faço aniversario em {dia} no mes {mes} do  {ano}")
+
+
+
+
