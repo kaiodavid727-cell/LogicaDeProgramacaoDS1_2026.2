1cc8da5f4d0bac1a591dc2b5a09ce088a94f4bae
kaiodavid727-cell <kaiodavid727@gmail.com>
2026-09-17 13:44:34 -0300

    atividade mao na massa 1

---
+++ 02_conceitos_basicos/atividade_mao_na_massa-1.py
@@ -0,0 +1,13 @@
+# TODO: Declare as variáveis abaixo e execute a célula para testar
+nome_produto = "cafe"
+quantidade_estoque = 50
+preco_unitario = 8.0
+disponivel_para_venda = True
+
+# TODO: Escreva os prints de inspeção aqui
+print("nome_produto",type(nome_produto))
+print("quantidade_estoque",type(quantidade_estoque))
+print("preco_unitario",type(preco_unitario))
+print("dispenivel_para_venda",type(True))
+
+      
--- 02_conceitos_basicos/pratica-aluno-mao-na-massa_2.py
+++ 02_conceitos_basicos/pratica-aluno-mao-na-massa_2.py
@@ -3,10 +3,7 @@
 # 2. Leia o número de pessoas (int)
 # 3. Calcule o valor por pessoa
 # 4. Imprima formatado usando f-string
-print=[50]
-print=[5]
-print=[10]
-valor= input("ler o valor da conta")
+
 pessoas= input("digitar o numero de pessoas")
 valor_pessoa= input("calcule o valor por pessoa")
 print=(f"valor da conta (int){valor} quantidade de pessoas (int){pessoas} valor por pessoa (int){valor_pessoa} ")
